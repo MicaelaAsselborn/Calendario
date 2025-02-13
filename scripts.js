6f758fe0be66e85251e4ee78e8233ac114d43a9e
Micaela Asselborn <asselborn96@gmail.com>
2025-02-13 20:17:34 -0300

    days showing properly now

--- scripts.js
+++ scripts.js
@@ -7,15 +7,14 @@ const month = date.getMonth();
 const year = date.getFullYear();
 const lastDay = new Date(year, month + 1, 0).getDate();
 const prevMonthLastDay = new Date(year, month, 0).getDate();
-
-console.log(prevMonthLastDay)
+const nextDaysIndex = 7 - day - 1;
 
 //ARRAYS
 const monthName = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agoto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
 ];
 const weekday = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
 
-//QUERY
+//QUERY SELECTOR
 const monthText = document.querySelector("#month");
 const yearText = document.querySelector("#year");
 const dateText = document.querySelector("#current-date");
@@ -27,13 +26,18 @@ yearText.innerText = year;
 dateText.innerText = weekday[day] + " " + numberDate + " de " + monthName[month] + ", " + year
 
 //FUNCTIONS
-let days = ""
+let days = "";
 
 for (i = day + 1; i > 0; i--){
-    days += `<div class="prev-days">${prevMonthLastDay - i}</div>`
-};
+    days += `<div class="prev-days">${prevMonthLastDay - i + 1}</div>`
+}; //CALCULATES PREV MONTH DAYS TO SHOW
 
 for (i = 1; i <= lastDay; i++){
     days += `<div>${i}</div>`
     currentMonthDate.innerHTML = days
-}   
+}   //CALCULATES CURRENT MONTH DAYS TO SHOW
+
+for (i = 1; i <= nextDaysIndex; i++){
+    days += `<div class="next-days">${i}</div>`
+    currentMonthDate.innerHTML = days
+} //CALCULATES NEXT MONTH DAYS TO SHOW
