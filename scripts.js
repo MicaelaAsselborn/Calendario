302bd49d0ec81021de2be15f9d71f558ece694e2
Micaela Asselborn <asselborn96@gmail.com>
2025-02-13 18:50:55 -0300

    prev and current days correctly displayed

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 //CONSTANTES
 const date = new Date();
+
 const day = date.getDay();
 const numberDate = date.getDate();
 const month = date.getMonth();
 const year = date.getFullYear();
+const lastDay = new Date(year, month + 1, 0).getDate();
+const prevMonthLastDay = new Date(year, month, 0).getDate();
+
+console.log(prevMonthLastDay)
 
 //ARRAYS
 const monthName = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agoto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
@@ -11,13 +16,24 @@ const monthName = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio
 const weekday = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"];
 
 //QUERY
-const monthText = document.querySelector("#month")
-const yearText = document.querySelector("#year")
-const dateText = document.querySelector("#current-date")
+const monthText = document.querySelector("#month");
+const yearText = document.querySelector("#year");
+const dateText = document.querySelector("#current-date");
+const currentMonthDate = document.querySelector("#days")
 
 //DATES
 monthText.innerText = monthName[month];
 yearText.innerText = year;
 dateText.innerText = weekday[day] + " " + numberDate + " de " + monthName[month] + ", " + year
 
-//DAYS
+//FUNCTIONS
+let days = ""
+
+for (i = day + 1; i > 0; i--){
+    days += `<div class="prev-days">${prevMonthLastDay - i}</div>`
+};
+
+for (i = 1; i <= lastDay; i++){
+    days += `<div>${i}</div>`
+    currentMonthDate.innerHTML = days
+}   
