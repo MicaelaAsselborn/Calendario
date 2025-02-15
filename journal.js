ed83891ddd5f219a95cad9d7adc6ab9487d2da42
Micaela Asselborn <asselborn96@gmail.com>
2025-02-15 20:37:20 -0300

    note and event modal working. close button working

--- journal.js
+++ journal.js
@@ -1,7 +1,13 @@
 function addEvent(){
-document.querySelector(".background").style.display = "block";
-document.querySelector("#event-creation").style.display = "block";
+    document.querySelector(".background").style.display = "block";
+    document.querySelector("#event-creation").style.display = "block";
+}
+function closeDiv(){
+    document.querySelector(".background").style.display = "none";
+    document.querySelector("#event-creation").style.display = "none";
+    document.querySelector("#note-creation").style.display = "none";
 }
 function addNote(){
-
+    document.querySelector(".background").style.display = "block";
+    document.querySelector("#note-creation").style.display = "block";
 }
